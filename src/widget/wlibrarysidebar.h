1ef6ae54711e59b74dbc3485b5bbd81f3de848ff
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2024-05-18 21:52:16 +0000

    WLibrarySidebar: Clean up drag'n'drop code and reenable drop indicators

--- src/widget/wlibrarysidebar.h
+++ src/widget/wlibrarysidebar.h
@@ -18,7 +18,9 @@ class WLibrarySidebar : public QTreeView, public WBaseWidget {
     void contextMenuEvent(QContextMenuEvent * event) override;
     void dragMoveEvent(QDragMoveEvent * event) override;
     void dragEnterEvent(QDragEnterEvent * event) override;
+    void dragLeaveEvent(QDragLeaveEvent* event) override;
     void dropEvent(QDropEvent * event) override;
+    void paintEvent(QPaintEvent* event) override;
     void keyPressEvent(QKeyEvent* event) override;
     void mousePressEvent(QMouseEvent* event) override;
     void focusInEvent(QFocusEvent* event) override;
@@ -43,6 +45,9 @@ class WLibrarySidebar : public QTreeView, public WBaseWidget {
     bool event(QEvent* pEvent) override;
 
   private:
+    void paintDropIndicator(QPainter& painter);
+    QRect m_dropIndicatorRect;
+
     void focusSelectedIndex();
     QModelIndex selectedIndex();
 
