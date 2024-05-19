b4560fcf0c2e9b30bc943959782ce5a38de7aabc
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2024-05-19 12:37:59 +0000

    WLibrarySidebar: Track hover times

--- src/widget/wlibrarysidebar.h
+++ src/widget/wlibrarysidebar.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QBasicTimer>
+#include <QElapsedTimer>
 #include <QModelIndex>
 #include <QTreeView>
 
@@ -49,6 +50,8 @@ class WLibrarySidebar : public QTreeView, public WBaseWidget {
     void focusSelectedIndex();
     QModelIndex selectedIndex();
 
+    QElapsedTimer m_activationTimer;
+    QElapsedTimer m_expirationTimer;
     QModelIndex m_autoExpandIndex;
 
     WLongHoverTracker<QModelIndex> m_longHover;
