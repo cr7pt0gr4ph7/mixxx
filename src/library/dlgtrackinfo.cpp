40cf34264e330535578ea8095af39895a6d5cbd7
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2024-05-25 17:24:06 +0000

    Display placeholder for last played date

--- src/library/dlgtrackinfo.cpp
+++ src/library/dlgtrackinfo.cpp
@@ -420,10 +420,10 @@ void DlgTrackInfo::replaceTrackRecord(
             mixxx::displayLocalDateTime(
                     mixxx::localDateTimeFromUtc(
                             m_trackRecord.getDateAdded())));
-    txtDateLastPlayed->setText(
-            mixxx::displayLocalDateTime(
-                    mixxx::localDateTimeFromUtc(
-                            m_trackRecord.getPlayCounter().getLastPlayedAt())));
+    auto lastPlayed = m_trackRecord.getPlayCounter().getLastPlayedAt();
+    txtDateLastPlayed->setText(lastPlayed.isValid()
+                    ? mixxx::displayLocalDateTime(mixxx::localDateTimeFromUtc(lastPlayed))
+                    : QStringLiteral("-"));
 
     QFileInfo info(trackLocation);
     if (info.exists() && info.isFile()) {
