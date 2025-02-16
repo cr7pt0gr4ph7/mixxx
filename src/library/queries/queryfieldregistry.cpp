5f851141ecaedac098a46abaa7251f71e08a5bd2
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-02-16 21:22:11 +0100

    Update queryfieldregistry.cpp

--- src/library/queries/queryfieldregistry.cpp
+++ src/library/queries/queryfieldregistry.cpp
@@ -6,7 +6,8 @@
 QList<QueryFieldInfo> QueryFieldRegistry::s_defaultFields = {
         QueryFieldInfo(
                 QStringLiteral("simple_query"),
-                QueryFieldType::Text),
+                QueryFieldType::Text,
+                {}),
         QueryFieldInfo(
                 QStringLiteral("artist"),
                 QueryFieldType::Text,
@@ -57,17 +58,17 @@ QList<QueryFieldInfo> QueryFieldRegistry::s_defaultFields = {
         QueryFieldInfo(
                 QStringLiteral("key"),
                 QueryFieldType::ChromaticKey,
-                { LIBRARYTABLE_KEY}),
+                { LIBRARYTABLE_KEY }),
         QueryFieldInfo(
                 QStringLiteral("key_id"),
                 // FIXME(cr7pt0gr4ph7): Check type of key_id
                 QueryFieldType::Text,
-                { LIBRARYTABLE_KEY_ID}),
-                        QueryFieldInfo(
+                { LIBRARYTABLE_KEY_ID }),
+        QueryFieldInfo(
                 QStringLiteral("id"),
                 // FIXME(cr7pt0gr4ph7): Check type of id
                 QueryFieldType::Text,
-                { LIBRARYTABLE_ID}),
+                { LIBRARYTABLE_ID }),
         QueryFieldInfo(
                 QStringLiteral("bpm"),
                 QueryFieldType::Bpm,
@@ -79,19 +80,19 @@ QList<QueryFieldInfo> QueryFieldRegistry::s_defaultFields = {
         QueryFieldInfo(
                 QStringLiteral("datetime_added"),
                 QueryFieldType::Date,
-                { LIBRARYTABLE_DATETIMEADDED ),
+                { LIBRARYTABLE_DATETIMEADDED }),
         QueryFieldInfo(
                 QStringLiteral("last_played_at"),
                 QueryFieldType::Date,
-                {LIBRARYTABLE_LAST_PLAYED_AT),
+                { LIBRARYTABLE_LAST_PLAYED_AT }),
         QueryFieldInfo(
                 QStringLiteral("played"),
                 QueryFieldType::Number,
-                { LIBRARYTABLE_PLAYED}),
+                { LIBRARYTABLE_PLAYED }),
         QueryFieldInfo(
                 QStringLiteral("timesplayed"),
                 QueryFieldType::Number,
-                { LIBRARYTABLE_TIMESPLAYED}),
+                { LIBRARYTABLE_TIMESPLAYED }),
         QueryFieldInfo(
                 QStringLiteral("rating"),
                 QueryFieldType::Number,
@@ -103,7 +104,7 @@ QList<QueryFieldInfo> QueryFieldRegistry::s_defaultFields = {
         QueryFieldInfo(
                 QStringLiteral("track_in"), // aka. track:
                 QueryFieldType::Track,
-                { /* Requires special handling */ }),),
+                { /* Requires special handling */ }),
         QueryFieldInfo(
                 QStringLiteral("playlist"),
                 QueryFieldType::Playlist,
@@ -119,63 +120,68 @@ QList<QueryFieldInfo> QueryFieldRegistry::s_defaultFields = {
 };
 
  QueryFieldCategory(const QString& name, const QString& localizedName, const QList<QueryFieldInfo>& fields)
- : m_name(name), m_localizedName(localizedName), m_fields(fields){
+   : m_name(name),
+     m_localizedName(localizedName),
+     m_fields(fields) {
  }
 
-    QueryFieldRegistry::QueryFieldRegistry(): m_defaultFieldsAdded(false) {}
+QueryFieldRegistry::QueryFieldRegistry()
+  : m_defaultFieldsAdded(false) {
+}
 
-    QueryFieldRegistry::QueryFieldRegistry(const QList<QString>& fields) : m_defaultFieldsAdded(false) {
-        addFields(fields);
+QueryFieldRegistry::QueryFieldRegistry(const QList<QString>& fields)
+  : m_defaultFieldsAdded(false) {
+    addFields(fields);
+}
+
+QueryFieldInfo QueryFieldRegistry::get(const QString& name) const {
+    auto lookupResult = m_nameToField.constFind(name);
+    if (lookupResult == m_nameToField.constEnd()) {
+        return QueryFieldInfo(name, QueryFieldType::Invalid, QList());
     }
+    return lookupResult.value();
+}
 
-    const QueryFieldInfo QueryFieldRegistry::get(const QString& name) const {
-        auto lookupResult = m_nameToField.constFind(name);
-        if (lookupResult == m_nameToField.constEnd()) {
-            return QueryFieldInfo(name, QueryFieldType::Invalid, QList());
+QList<QString> QueryFieldRegistry::allFields() const {
+    return m_allFields;
+}
+
+QList<QueryFieldCategory> QueryFieldRegistry::allFieldsByCategory() const {
+    // TODO(cr7pt0gr4ph7): Group the defined fields into the following categories:
+    // - Ungrouped: Simple search in all main track fields
+    // - "Track Fields": Album, Artist, Title, Album Artist
+    // - "Additional Track Fields": Rating, Genre, Grouping, Comment, Year, ...
+    // - "File Info": Bitrate, Filetype, (Duration?), Location
+    // - "Library Metadata" Date Added, Times Played, Last Played At, ...
+    // - "Custom": Custom fields, if these are ever implemented
+    return { QueryFieldCategory("all", "All Fields", allFields()) };
+}
+
+void QueryFieldRegistry::addFields(const QList<QueryFieldInfo>& fields) {
+    for (const auto& field : fields) {
+        if (field.name().isEmpty()) {
+            qWarning() << "QueryFieldRegistry: Skipping field with empty name.";
+            continue;
         }
-        return lookupResult.value();
-    }
-
-    const QList<QString> QueryFieldRegistry::allFields() const {
-        return m_allFields;
-    }
-
-    const QList<QueryFieldCategory> QueryFieldRegistry::allFieldsByCategory() const {
-        // TODO(cr7pt0gr4ph7): Group the defined fields into the following categories:
-        // - Ungrouped: Simple search in all main track fields
-        // - "Track Fields": Album, Artist, Title, Album Artist
-        // - "Additional Track Fields": Rating, Genre, Grouping, Comment, Year, ...
-        // - "File Info": Bitrate, Filetype, (Duration?), Location
-        // - "Library Metadata" Date Added, Times Played, Last Played At, ...
-        // - "Custom": Custom fields, if these are ever implemented
-        return {QueryFieldCategory("all", "All Fields", allFields())};
-    }
-
-        void QueryFieldRegistry::addFields(const QList<QueryFieldInfo>& fields) {
-        for (const auto& field : fields) {
-            if (field.name().isEmpty()) {
-                qWarning() << "QueryFieldRegistry: Skipping field with empty name.";
-                continue;
-            }
-            if (field.type().kind() == QueryFieldType::Invalid) {
-                qWarning() << "QueryFieldRegistry: Skipping field with invalid type";
-                continue;
-            }
-            if (m_nameToField.contains(field.name())) {
-                qWarning() << "QueryFieldRegistry: Skipping field with duplicate name.";
-                continue;
-            }
-            m_nameToField.insert(field.name(), field);
-            m_allFields.append(field);
+        if (field.type().kind() == QueryFieldType::Invalid) {
+            qWarning() << "QueryFieldRegistry: Skipping field with invalid type";
+            continue;
         }
-    }
-
-    void QueryFieldRegistry::addDefaultFields() {
-        VERIFY_OR_DEBUG_ASSERT(!m_defaultFieldsAdded) {
-            qWarning() << "QueryFieldRegistry::addDefaultFields() has already "
-                          "been called on this instance.";
-            return;
+        if (m_nameToField.contains(field.name())) {
+            qWarning() << "QueryFieldRegistry: Skipping field with duplicate name.";
+            continue;
         }
-        addFields(s_defaultFields);
-        m_defaultFieldsAdded = true;
+        m_nameToField.insert(field.name(), field);
+        m_allFields.append(field);
     }
+}
+
+void QueryFieldRegistry::addDefaultFields() {
+    VERIFY_OR_DEBUG_ASSERT(!m_defaultFieldsAdded) {
+        qWarning() << "QueryFieldRegistry::addDefaultFields() has already "
+                      "been called on this instance.";
+        return;
+    }
+    addFields(s_defaultFields);
+    m_defaultFieldsAdded = true;
+}
