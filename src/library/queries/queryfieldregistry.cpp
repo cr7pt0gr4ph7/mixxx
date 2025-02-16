6097b2459dfcb62a89eb2ac02ef58a2f651bd179
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-02-16 12:17:24 +0000

    QueryFieldRegistry: Define fields available in search queries.

---
+++ src/library/queries/queryfieldregistry.cpp
@@ -0,0 +1,181 @@
+#include "library/queries/queryfieldregistry.h"
+
+#include "library/dao/trackschema.h"
+#include "util/assert.h"
+
+QList<QueryFieldInfo> QueryFieldRegistry::s_defaultFields = {
+        QueryFieldInfo(
+                QStringLiteral("simple_query"),
+                QueryFieldType::Text),
+        QueryFieldInfo(
+                QStringLiteral("artist"),
+                QueryFieldType::Text,
+                { LIBRARYTABLE_ARTIST, LIBRARYTABLE_ALBUMARTIST }),
+        QueryFieldInfo(
+                QStringLiteral("album_artist"),
+                QueryFieldType::Text,
+                { LIBRARYTABLE_ALBUMARTIST }),
+        QueryFieldInfo(
+                QStringLiteral("album"),
+                QueryFieldType::Text,
+                { LIBRARYTABLE_ALBUM }),
+        QueryFieldInfo(
+                QStringLiteral("title"),
+                QueryFieldType::Text,
+                { LIBRARYTABLE_TITLE }),
+        QueryFieldInfo(
+                QStringLiteral("genre"),
+                QueryFieldType::Text,
+                { LIBRARYTABLE_GENRE }),
+        QueryFieldInfo(
+                QStringLiteral("composer"),
+                QueryFieldType::Text,
+                { LIBRARYTABLE_COMPOSER }),
+        QueryFieldInfo(
+                QStringLiteral("grouping"),
+                QueryFieldType::Text
+                { LIBRARYTABLE_GROUPING }),
+        QueryFieldInfo(
+                QStringLiteral("comment"),
+                QueryFieldType::Text
+                { LIBRARYTABLE_COMMENT }),
+        QueryFieldInfo(
+                QStringLiteral("type"), // aka. filetype
+                QueryFieldType::Text,
+                { LIBRARYTABLE_FILETYPE }),
+        QueryFieldInfo(
+                QStringLiteral("location"),
+                QueryFieldType::Text),
+        QueryFieldInfo(
+                QStringLiteral("year"),
+                QueryFieldType::Year,
+                { LIBRARYTABLE_YEAR }),
+        QueryFieldInfo(
+                QStringLiteral("duration"),
+                QueryFieldType::Duration,
+                { LIBRARYTABLE_DURATION }),
+        QueryFieldInfo(
+                QStringLiteral("key"),
+                QueryFieldType::ChromaticKey,
+                { LIBRARYTABLE_KEY}),
+        QueryFieldInfo(
+                QStringLiteral("key_id"),
+                // FIXME(cr7pt0gr4ph7): Check type of key_id
+                QueryFieldType::Text,
+                { LIBRARYTABLE_KEY_ID}),
+                        QueryFieldInfo(
+                QStringLiteral("id"),
+                // FIXME(cr7pt0gr4ph7): Check type of id
+                QueryFieldType::Text,
+                { LIBRARYTABLE_ID}),
+        QueryFieldInfo(
+                QStringLiteral("bpm"),
+                QueryFieldType::Bpm,
+                { LIBRARYTABLE_BPM }),
+        QueryFieldInfo(
+                QStringLiteral("bitrate"),
+                QueryFieldType::Number,
+                { LIBRARYTABLE_BITRATE }),
+        QueryFieldInfo(
+                QStringLiteral("datetime_added"),
+                QueryFieldType::Date,
+                { LIBRARYTABLE_DATETIMEADDED ),
+        QueryFieldInfo(
+                QStringLiteral("last_played_at"),
+                QueryFieldType::Date,
+                {LIBRARYTABLE_LAST_PLAYED_AT),
+        QueryFieldInfo(
+                QStringLiteral("played"),
+                QueryFieldType::Number,
+                { LIBRARYTABLE_PLAYED}),
+        QueryFieldInfo(
+                QStringLiteral("timesplayed"),
+                QueryFieldType::Number,
+                { LIBRARYTABLE_TIMESPLAYED}),
+        QueryFieldInfo(
+                QStringLiteral("rating"),
+                QueryFieldType::Number,
+                { LIBRARYTABLE_RATING }),
+        QueryFieldInfo(
+                QStringLiteral("track"), /// aka. tracknumber
+                QueryFieldType::Number,
+                { LIBRARYTABLE_TRACKNUMBER }),
+        QueryFieldInfo(
+                QStringLiteral("track_in"), // aka. track:
+                QueryFieldType::Track,
+                { /* Requires special handling */ }),),
+        QueryFieldInfo(
+                QStringLiteral("playlist"),
+                QueryFieldType::Playlist,
+                { /* Requires special handling */ }),
+        QueryFieldInfo(
+                QStringLiteral("crate"),
+                QueryFieldType::Crate,
+                { /* Requires special handling */ }),
+        QueryFieldInfo(
+                QStringLiteral("history"),
+                QueryFieldType::History,
+                { /* Requires special handling */ }),
+};
+
+ QueryFieldCategory(const QString& name, const QString& localizedName, const QList<QueryFieldInfo>& fields)
+ : m_name(name), m_localizedName(localizedName), m_fields(fields){
+ }
+
+    QueryFieldRegistry::QueryFieldRegistry(): m_defaultFieldsAdded(false) {}
+
+    QueryFieldRegistry::QueryFieldRegistry(const QList<QString>& fields) : m_defaultFieldsAdded(false) {
+        addFields(fields);
+    }
+
+    const QueryFieldInfo QueryFieldRegistry::get(const QString& name) const {
+        auto lookupResult = m_nameToField.constFind(name);
+        if (lookupResult == m_nameToField.constEnd()) {
+            return QueryFieldInfo(name, QueryFieldType::Invalid, QList());
+        }
+        return lookupResult.value();
+    }
+
+    const QList<QString> QueryFieldRegistry::allFields() const {
+        return m_allFields;
+    }
+
+    const QList<QueryFieldCategory> QueryFieldRegistry::allFieldsByCategory() const {
+        // TODO(cr7pt0gr4ph7): Group the defined fields into the following categories:
+        // - Ungrouped: Simple search in all main track fields
+        // - "Track Fields": Album, Artist, Title, Album Artist
+        // - "Additional Track Fields": Rating, Genre, Grouping, Comment, Year, ...
+        // - "File Info": Bitrate, Filetype, (Duration?), Location
+        // - "Library Metadata" Date Added, Times Played, Last Played At, ...
+        // - "Custom": Custom fields, if these are ever implemented
+        return {QueryFieldCategory("all", "All Fields", allFields())};
+    }
+
+        void QueryFieldRegistry::addFields(const QList<QueryFieldInfo>& fields) {
+        for (const auto& field : fields) {
+            if (field.name().isEmpty()) {
+                qWarning() << "QueryFieldRegistry: Skipping field with empty name.";
+                continue;
+            }
+            if (field.type().kind() == QueryFieldType::Invalid) {
+                qWarning() << "QueryFieldRegistry: Skipping field with invalid type";
+                continue;
+            }
+            if (m_nameToField.contains(field.name())) {
+                qWarning() << "QueryFieldRegistry: Skipping field with duplicate name.";
+                continue;
+            }
+            m_nameToField.insert(field.name(), field);
+            m_allFields.append(field);
+        }
+    }
+
+    void QueryFieldRegistry::addDefaultFields() {
+        VERIFY_OR_DEBUG_ASSERT(!m_defaultFieldsAdded) {
+            qWarning() << "QueryFieldRegistry::addDefaultFields() has already "
+                          "been called on this instance.";
+            return;
+        }
+        addFields(s_defaultFields);
+        m_defaultFieldsAdded = true;
+    }
---
+++ src/library/queries/queryfieldregistry.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <QList>
+#include <QMap>
+#include <QString>
+#include <memory>
+
+#include "library/queries/queryfield.h"
+
+/// Represents a group of related query fields. This should only be used
+/// for the purpose of presenting these logical groups within the UI.
+///
+/// Values of this class are immutable.
+///
+/// NOTE(cr7pt0gr4ph7): For simplicity, you should be able to pass
+/// QueryFieldCategory around as a value. Consumers do not (and should not) have
+/// to care about the underlying object identities. For reduced memory
+/// consumption we might explore implicit sharing using internal pointers.
+class QueryFieldCategory {
+  public:
+    QueryFieldCategory(const QString& name,
+            const QString& localizedName,
+            const QList<QueryFieldInfo>& fields);
+
+    /// Gets the internal name of this group.
+    QString name() const {
+        // FIXME(cr7pt0gr4ph7): Translate the name using tr("...")
+        return m_name;
+    }
+
+    /// Gets the localized display name of this group.
+    QString localizedName() const {
+        // FIXME(cr7pt0gr4ph7): Translate the name using tr("...")
+        return m_localizedName;
+    }
+
+    /// Gets the fields that belong to this group.
+    const QList<QueryFieldInfo> fields() const {
+        return m_fields;
+    }
+
+  private:
+    QString m_name;
+    QString m_localizedName;
+    QList<QueryFieldInfo> m_fields;
+}
+
+/// Provides access to the list of fields that are available
+/// for use in search queries.
+class QueryFieldRegistry {
+  public:
+    QueryFieldRegistry();
+    QueryFieldRegistry(const QList<QString>& fields);
+
+    /// Gets the field with the specified name.
+    ///
+    /// Returns a QueryFieldInfo with QueryFieldType::Invalid when the specified
+    /// name does not refer to a known field.
+    const QueryFieldInfo get(const QString& name) const;
+
+    /// Returns a list of all available fields.
+    const QList<QueryFieldInfo> allFields() const;
+
+    /// Returns a list of all fields grouped into logical groups.
+    const QList<QueryFieldGroup> allFieldsByGroup() const;
+
+    /// Adds the default fields to this registry.
+    ///
+    /// Should only be called at most once on a given instance.
+    ///
+    /// Not thread-safe.
+    void addDefaultFields();
+
+  private:
+    void addFields(const QList<QueryFieldInfo> fields);
+
+    static QList<QueryFieldInfo> s_defaultFields;
+    QList<QueryFieldInfo> m_allFields;
+    QMap<QString, QueryFieldInfo> m_nameToField;
+    bool m_defaultFieldsAdded;
+};
